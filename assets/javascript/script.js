56ca089b03af2e7533f4443b2524b02a6372e1db
Alba Cisneros <cisnerosan86@gmail.com>
2018-06-29 20:02:48 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,12 +4,13 @@
   const scoreDisplay = $('h3#score');
   const game = {
     init: function() {
+     
       const numberDisplay = $('span#number');
       const crystals = [
-        $('img.gold'),
-        $('img.blue'),
-        $('img.purple'),
-        $('img.green')
+        $('img.heart'),
+        $('img.square'),
+        $('img.tear'),
+        $('img.triangle')
       ];
       score = 0;
       scoreDisplay.text(score);
@@ -17,33 +18,36 @@
       numberDisplay.text(number);
       const values = [];
       let uniqueValues = [];
-      //Get 4 unique values
       while (uniqueValues.length < crystals.length) {
         let value = Math.floor(Math.random() * 11) + 1;
         values.push(value);
         uniqueValues = Array.from(new Set(values));
       }
-      //Assign value to each crystal
       for (let i = 0; i < crystals.length; i++) {
         crystals[i].attr('data-value', uniqueValues[i]);
-        console.log(crystals[i][0].dataset.value);
+        console.log('crystals from for loop: ', crystals[i][0].dataset.value);
       }
     },
     play: function() {
         let wins = 0;
         let losses = 0;
         const crystalsDiv = $('div.crystals');
+        console.log('crystalDiv: ', crystalsDiv);
         const message = $('h3#message');
         crystalsDiv.on('click', '.crystal', function() {
+         
           const value = parseInt(this.dataset.value);
           score += value;
           scoreDisplay.text(score);
+          console.log(score,number);
           if (score === number) {
             message.text('You Win!');
             wins++;
             $('#wins').text(wins);
             game.init();
           } else if (score > number) {
+            console.log('message.text', message.text);
+            console.log('losses',losses);
             message.text('You Lose!');
             losses++;
             $('#losses').text(losses);
@@ -53,8 +57,10 @@
           }
         });
       }
+      
     };
     game.init();
     game.play();
+
   })();
   
